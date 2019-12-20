312fc246a92de7d7ce581253cf0dbd0b58cd5422
RabinMaharjan00 <aloneme0015@gmail.com>
2019-12-20 20:18:37 +0545

    initial angular commit

---
+++ project/src/app/localserver/localserver.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {GetFile} from '../getFile';
+
+@Component({
+  selector: 'app-localserver',
+  templateUrl: './localserver.component.html',
+  styleUrls: ['./localserver.component.css']
+})
+export class LocalserverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
